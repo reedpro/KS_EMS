d04778d5b10445afd5e3cf09e85a3b49839e2e02
reed_pro <poirier.jordan@gmail.com>
2014-12-02 13:40:25 -0500

    Added validation class
    Currently validates Names, checking for numbers

---
+++ Supporting/Validation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supporting
+{
+    /// <summary>
+    /// Validation Class to validate all input
+    /// </summary>
+    class Validation
+    {
+        /// <summary>
+        /// Gets set each time the validation fails, detailing why it failed
+        /// </summary>
+        public string errorMsg { get; private set; }
+
+        /// <summary>
+        /// Validates a name string to ensure that it has no numbers and only accepted symbols
+        /// </summary>
+        /// <param name="inName">String to be validated</param>
+        /// <returns>Bool indicating success</returns>
+        public bool name(string inName)
+        {
+            bool returnVal = false;
+            if(inName.Any(c => char.IsDigit(c)))
+            {
+                errorMsg = "A name cannot contain any numbers";
+            }
+            else
+            {
+                returnVal = true;
+            }
+            return returnVal;
+        }
+    }
+}
