9e3cb8f29f94d055896558d9ac7bda3651604a80
jmoorhouse-cc <jmoorhouse-cc@conestogac.on.ca>
2014-12-04 16:39:24 -0500

    filestream progress in FileIO

--- Supporting/FileIO.cs
+++ Supporting/FileIO.cs
@@ -16,6 +16,7 @@ namespace Supporting
     public class FileIO
     {
         string dbasePath = Directory.GetCurrentDirectory();
+        string dbFilePath = Path.Combine(Directory.GetCurrentDirectory(), "DBase", "DB.csv");
 
         /// <summary>
         /// Holds a database file for reading
@@ -28,16 +29,27 @@ namespace Supporting
         private StreamWriter dBase_W;
 
         /// <summary>
-        /// Opens a file for reading based on a path provided
+        /// Opens a file from the DBase folder of the application and read in each 
+        /// pipe-delimited value to a string array to return to caller until newline.
         /// </summary>
         /// <param name="path">A string containing the path of the file to open</param>
         public void dBaseOpen_R()
         {
-            if (File.Exists(dbasePath))
+            // if the file exists, open it
+            if (File.Exists(dbFilePath))
             {
-                //open the file for reading, making sure it exists
+                string[] fileLines;
+                char[] newLine = new char[2] {'\n','\r'};
+
+                //open the file for reading
                 dBase_R = new StreamReader(File.OpenRead(dbasePath));
-                // extract dbase info                
+                
+                // extract dbase info 
+                using (dBase_R)
+                {
+                    fileLines = dBase_R.ReadToEnd().Split(newLine);
+                }
+                dBase_R.Close();
             }
             else
             {
@@ -46,7 +58,8 @@ namespace Supporting
         }
 
         /// <summary>
-        /// Opens a file for writing based on a path provided
+        /// Opens a file for writing in the DBase folder of the application directory, and write
+        /// the parameter string array elements to the file, delimited by pipes.
         /// </summary>
         /// <param name="path">A string containing the path of the file to open</param>
         public void dBaseOpen_W(string[] employeeData)
@@ -56,9 +69,21 @@ namespace Supporting
             {
                 Directory.CreateDirectory(Path.Combine(dbasePath, "DBase"));
             }
-            //open file for writing, creating it if it doesnt exist TODO
-            dBase_W = new StreamWriter(File.OpenWrite(Path.Combine(dbasePath, "Dbase", "DB.csv")));
-            // write to dbase file
+
+            // open file for writing, creating it if it doesnt exist TODO
+            dBase_W = new StreamWriter(File.OpenWrite(dbFilePath));
+
+            // write to dbase file (formatted)
+            using (dBase_W)
+            {
+                // write each string in the array to the file, separated by pipes
+                foreach (string s in employeeData)
+                {
+                    dBase_W.Write(s + "|");
+                }
+                dBase_W.Write("\n"); // end of current entry, add new line
+                dBase_W.Close();
+            }
         }
     }
 }
