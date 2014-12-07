0768abe4a5ae6700ba6167b3b935964cca614d16
ThomTaylor <ThomLast666@gmail.com>
2014-12-07 15:38:03 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	EMS.v12.suo
    	EMS/bin/Debug/EMS.exe
    	EMS/bin/Debug/EMS.pdb
    	EMS/bin/Debug/Presentation.dll
    	EMS/bin/Debug/Presentation.pdb
    	EMS/obj/Debug/EMS.csprojResolveAssemblyReference.cache
    	EMS/obj/Debug/EMS.exe
    	EMS/obj/Debug/EMS.pdb
    	Presentation/bin/Debug/Presentation.dll
    	Presentation/bin/Debug/Presentation.pdb
    	Presentation/obj/Debug/Presentation.dll

--- UnitTest_ContractEmployee/UnitTest_FullTimeEmployee.cs
+++ UnitTest_ContractEmployee/UnitTest_FullTimeEmployee.cs
@@ -34,6 +34,13 @@ namespace UnitTest_AllEmployees
         [TestMethod]
         public void SetDateOfHire_NormalTest1()
         {
+            string input = "11/31/2014";
+            bool expected = true;
+            bool actual = false;
+
+            FulltimeEmployee fulE = new FulltimeEmployee();
+            actual = fulE.SetDateOfHire(input);
+            Assert.AreEqual(expected, actual, "Did not accept valid date of hire");
 
         }
 
@@ -42,15 +49,21 @@ namespace UnitTest_AllEmployees
         /// <para><b>Unique Identifier</b> - AE.FTE.SDT.N.1</para>
         /// <para><b>Description</b> - Method tests the regular use of the method, attempting to set the dateOfTermination variable</para>
         /// <para><b>Method of execution</b> - Automatic</para>
-        /// <para><b>Input data</b> - "11/31/2014"</para>
-        /// <para><b>Expected outputs</b> - "11/31/2014" set correctly for variable: dateOfTermination</para>
-        /// <para><b>Observed outputs</b> - "11/31/2014" set correctly for variable: dateOfTermination</para>
+        /// <para><b>Input data</b> - "12/14/2014"</para>
+        /// <para><b>Expected outputs</b> - "12/14/2014" set correctly for variable: dateOfTermination</para>
+        /// <para><b>Observed outputs</b> - "12/14/2014" set correctly for variable: dateOfTermination</para>
         /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
         ///
         [TestMethod]
         public void SetDateOfTermination_NormalTest1()
         {
+            string input = "12/14/2014";
+            bool expected = true;
+            bool actual = false;
 
+            FulltimeEmployee fulE = new FulltimeEmployee();
+            actual = fulE.SetDateOfHire(input);
+            Assert.AreEqual(expected, actual, "Did not accept valid date of hire");
         }
 
         ///
@@ -67,6 +80,14 @@ namespace UnitTest_AllEmployees
         public void SetDateOfHire_ExceptionTest1()
         {
 
+            string input = "today";
+            bool expected = false;
+            bool actual = true;
+
+            FulltimeEmployee fulE = new FulltimeEmployee();
+            actual = fulE.SetDateOfHire(input);
+            Assert.AreEqual(expected, actual, "Allowed inproper date");
+
         }
 
         ///
@@ -74,14 +95,21 @@ namespace UnitTest_AllEmployees
         /// <para><b>Unique Identifier</b> - AE.FTE.SDT.E.1</para>
         /// <para><b>Description</b> - Method tests exceptional use of the method, attempting to set the dateOfTermination variable to illegal data type</para>
         /// <para><b>Method of execution</b> - Automatic</para>
-        /// <para><b>Input data</b> - "today"</para>
-        /// <para><b>Expected outputs</b> - "today" rejected as input</para>
-        /// <para><b>Observed outputs</b> - "today" rejected as input</para>
+        /// <para><b>Input data</b> - "Yesterday"</para>
+        /// <para><b>Expected outputs</b> - "false" rejected as input</para>
+        /// <para><b>Observed outputs</b> - "Yesterday" rejected as input</para>
         /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
         ///
         [TestMethod]
         public void SetDateOfTermination_ExceptionTest1()
         {
+            string input = "yesterday";
+            bool expected = false;
+            bool actual = true;
+
+            FulltimeEmployee fulE = new FulltimeEmployee();
+            actual = fulE.SetDateOfHire(input);
+            Assert.AreEqual(expected, actual, "Allowed inproper date");
 
         }
     }
