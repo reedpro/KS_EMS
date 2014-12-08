80556687d97e30252db4d20f04b41dfd48108990
ThomTaylor <ThomLast666@gmail.com>
2014-12-08 07:07:06 -0500

    spell check contract employee testing

--- UnitTest_ContractEmployee/UnitTest_ContractEmployee.cs
+++ UnitTest_ContractEmployee/UnitTest_ContractEmployee.cs
@@ -26,7 +26,7 @@ namespace UnitTest_AllEmployees
         /// <para><b>Description</b> - Method tests the regular use of the method</para>
         /// <para><b>Method of execution</b> - Automatic</para>
         /// <para><b>Input data</b> - "2014/12/03"</para>
-        /// <para><b>Expected outputs</b> - "2014/12/03" set correctly for SIN of that employee</para>
+        /// <para><b>Expected outputs</b> - "2014/12/03" A hypathetical start date</para>
         /// <para><b>Observed outputs</b> - passed</para>
         /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
         /// 
@@ -51,7 +51,7 @@ namespace UnitTest_AllEmployees
         /// <para><b>Description</b> - Method tests the regular use of the method</para>
         /// <para><b>Method of execution</b> - Automatic</para>
         /// <para><b>Input data</b> - "10/13/2014"</para>
-        /// <para><b>Expected outputs</b> - "2014/12/07" set correctly for SIN of that employee</para>
+        /// <para><b>Expected outputs</b> - "2014/12/07" </para>
         /// <para><b>Observed outputs</b> - passed</para>
         /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
         /// 
@@ -119,7 +119,7 @@ namespace UnitTest_AllEmployees
 
         ///
         /// <para><b>Test Identifier</b> - SetContractEndDate_NormalTest1()</para>
-        /// <para><b>Unique Identifier</b> - AE.E.SCEDDTDT.N.1</para>
+        /// <para><b>Unique Identifier</b> - AE.E.SCEDDT.N.1</para>
         /// <para><b>Description</b> - Method tests the regular use of the method</para>
         /// <para><b>Method of execution</b> - Automatic</para>
         /// <para><b>Input data</b> - "2014/12/03"</para>
@@ -193,7 +193,7 @@ namespace UnitTest_AllEmployees
 
         ///
         /// <para><b>Test Identifier</b> - SetFixedContractAmt_String_NormalTest1()</para>
-        /// <para><b>Unique Identifier</b> - AE.E.SFCAD.N.1</para>
+        /// <para><b>Unique Identifier</b> - AE.E.SFCAS.N.1</para>
         /// <para><b>Description</b> - Method tests the regular use of the method</para>
         /// <para><b>Method of execution</b> - Automatic</para>
         /// <para><b>Input data</b> - "15.00"</para>
@@ -242,7 +242,7 @@ namespace UnitTest_AllEmployees
         /// <para><b>Unique Identifier</b> - AE.E.SFCAD.N.1</para>
         /// <para><b>Description</b> - Method tests the regular use of the method</para>
         /// <para><b>Method of execution</b> - Automatic</para>
-        /// <para><b>Input data</b> - "15.00"</para>
+        /// <para><b>Input data</b> - "17.00"</para>
         /// <para><b>Expected outputs</b> - "True"</para>
         /// <para><b>Observed outputs</b> - passed</para>
         /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
@@ -251,7 +251,7 @@ namespace UnitTest_AllEmployees
         [TestMethod]
         public void SetFixedContractAmt_Decimal_NormalTest1()
         {
-            Decimal input = 15.00m;
+            Decimal input = 17.00m;
             bool expected = true;
             bool actual = false;
 
