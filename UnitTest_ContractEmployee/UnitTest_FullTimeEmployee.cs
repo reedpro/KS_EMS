68b92231eb69d99a57bea6dc99f49f5385517f7f
ThomTaylor <ThomLast666@gmail.com>
2014-12-08 02:38:52 -0500

    FullTime Employee Tests finailized

--- UnitTest_ContractEmployee/UnitTest_FullTimeEmployee.cs
+++ UnitTest_ContractEmployee/UnitTest_FullTimeEmployee.cs
@@ -110,8 +110,75 @@ namespace UnitTest_AllEmployees
             FulltimeEmployee fulE = new FulltimeEmployee();
             actual = fulE.SetDateOfHire(input);
             Assert.AreEqual(expected, actual, "Allowed inproper date");
-
         }
+
+        ///
+        /// <para><b>Test Identifier</b> - CheckSalary_NormalTest1()</para>
+        /// <para><b>Unique Identifier</b> - AE.FTE.CS.N.1</para>
+        /// <para><b>Description</b> - Method tests exceptional use of the method, attempting to set the dateOfTermination variable to illegal data type</para>
+        /// <para><b>Method of execution</b> - Automatic</para>
+        /// <para><b>Input data</b> - "13.00"</para>
+        /// <para><b>Expected outputs</b> - "13.00" rejected as input</para>
+        /// <para><b>Observed outputs</b> - Test Passes</para>
+        /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
+        ///
+        [TestMethod]
+        public void CheckSalary_NormalTest1()
+        {
+            Decimal input = 13.00m;
+            bool expected = true;
+            bool actual = false;
+
+            FulltimeEmployee fulE = new FulltimeEmployee();
+            actual = fulE.CheckSalary(input);
+            Assert.AreEqual(expected, actual, "Proper input rejected");
+        }
+
+        ///
+        /// <para><b>Test Identifier</b> - SetSalary_NormalTest1()</para>
+        /// <para><b>Unique Identifier</b> - AE.FTE.SS.N.1</para>
+        /// <para><b>Description</b> - Method tests the regular use of the method, attempting to set the dateOfTermination variable</para>
+        /// <para><b>Method of execution</b> - Automatic</para>
+        /// <para><b>Input data</b> - "13.00"</para>
+        /// <para><b>Expected outputs</b> - "13.00" set correctly for variable: SetSallary</para>
+        /// <para><b>Observed outputs</b> - Test passed </para>
+        /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
+        ///
+        [TestMethod]
+        public void SetSalary_NormalTest1()
+        {
+            Decimal input = 13.00m;
+            bool expected = true;
+            bool actual = false;
+
+            FulltimeEmployee fulE = new FulltimeEmployee();
+            actual = fulE.SetSalary(input);
+            Assert.AreEqual(expected, actual, "Did not accept valid salary");
+        }
+
+
+        ///
+        /// <para><b>Test Identifier</b> - SetSalary_NormalTest1()</para>
+        /// <para><b>Unique Identifier</b> - AE.FTE.SS.N.1</para>
+        /// <para><b>Description</b> - Method tests the regular use of the method, attempting to set the dateOfTermination variable</para>
+        /// <para><b>Method of execution</b> - Automatic</para>
+        /// <para><b>Input data</b> - "13.00"</para>
+        /// <para><b>Expected outputs</b> - "13.00" set correctly for variable: SetSallary</para>
+        /// <para><b>Observed outputs</b> - Test passed </para>
+        /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
+        ///
+        [TestMethod]
+        public void SetSalary_STRING_NormalTest1()
+        {
+            String input = "13.00";
+            bool expected = true;
+            bool actual = false;
+
+            FulltimeEmployee fulE = new FulltimeEmployee();
+            actual = fulE.SetSalary(input);
+            Assert.AreEqual(expected, actual, "Did not accept valid salary");
+        }
+
     }
 }
  
