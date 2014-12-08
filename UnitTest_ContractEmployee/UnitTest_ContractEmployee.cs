341a196160282d6bead68d91cdbd96f5601b1921
ThomTaylor <ThomLast666@gmail.com>
2014-12-08 04:47:14 -0500

    Contract Employee Testing Finalize

--- UnitTest_ContractEmployee/UnitTest_ContractEmployee.cs
+++ UnitTest_ContractEmployee/UnitTest_ContractEmployee.cs
@@ -71,6 +71,30 @@ namespace UnitTest_AllEmployees
 
 
         ///
+        /// <para><b>Test Identifier</b> - SetContractStartDate_NormalTest1()</para>
+        /// <para><b>Unique Identifier</b> - AE.E.SCSDDT.N.1</para>
+        /// <para><b>Description</b> - Method tests the regular use of the method</para>
+        /// <para><b>Method of execution</b> - Automatic</para>
+        /// <para><b>Input data</b> - "2014/12/03"</para>
+        /// <para><b>Expected outputs</b> - "2014/12/03" set Start Date</para>
+        /// <para><b>Observed outputs</b> - passed</para>
+        /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
+        /// 
+
+        [TestMethod]
+        public void SetContractStartDate_DateTime_NormalTest1()
+        {
+            DateTime input = new DateTime(2014 / 12 / 03);
+            bool expected = true;
+            bool actual = false;
+
+            ContractEmployee conE = new ContractEmployee();
+            actual = conE.SetContractStartDate(input);
+            Assert.AreEqual(expected, actual, "Did not accept valid contract start date");
+
+        }
+
+        ///
         /// <para><b>Test Identifier</b> - SetContractEndDate_ExceptionTest1()</para>
         /// <para><b>Unique Identifier</b> - AE.E.SCED.E.1</para>
         /// <para><b>Description</b> - Method tests the exceptional use of the method, attempting to set the FirstName variable with invalid format of string</para>
@@ -92,6 +116,31 @@ namespace UnitTest_AllEmployees
 
             Assert.AreEqual(expected, actual, "Accepted false contract end date");
         }
+
+        ///
+        /// <para><b>Test Identifier</b> - SetContractEndDate_NormalTest1()</para>
+        /// <para><b>Unique Identifier</b> - AE.E.SCEDDTDT.N.1</para>
+        /// <para><b>Description</b> - Method tests the regular use of the method</para>
+        /// <para><b>Method of execution</b> - Automatic</para>
+        /// <para><b>Input data</b> - "2014/12/03"</para>
+        /// <para><b>Expected outputs</b> - "2014/12/03" set end date</para>
+        /// <para><b>Observed outputs</b> - passed</para>
+        /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
+        /// 
+
+        [TestMethod]
+        public void SetContractEndDate_DateTime_NormalTest1()
+        {
+            DateTime input = new DateTime(2014 / 12 / 03);
+            bool expected = true;
+            bool actual = false;
+
+            ContractEmployee conE = new ContractEmployee();
+            actual = conE.SetContractEndDate(input);
+            Assert.AreEqual(expected, actual, "Did not accept valid contract start date");
+
+        }
+
 
         ///
         /// <para><b>Test Identifier</b> - SetContractEndDate_ExceptionTest1()</para>
@@ -116,5 +165,99 @@ namespace UnitTest_AllEmployees
             Assert.AreEqual(expected, actual, "Accepted false contract end date");
         }
 
+
+        ///
+        /// <para><b>Test Identifier</b> - CheckFixedContractAmt_String_NormalTest1()</para>
+        /// <para><b>Unique Identifier</b> - AE.E.CFCAD.N.1</para>
+        /// <para><b>Description</b> - Method tests the regular use of the method</para>
+        /// <para><b>Method of execution</b> - Automatic</para>
+        /// <para><b>Input data</b> - "15.00"</para>
+        /// <para><b>Expected outputs</b> - "True"</para>
+        /// <para><b>Observed outputs</b> - passed</para>
+        /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
+        /// 
+
+        [TestMethod]
+        public void CheckFixedContractAmt_String_NormalTest1()
+        {
+            string input = "15.00";
+            bool expected = true;
+            bool actual = false;
+
+            ContractEmployee conE = new ContractEmployee();
+            actual = conE.CheckFixedContractAmt(input);
+            Assert.AreEqual(expected, actual, "Did not accept valid contract start date");
+
+        }
+
+
+        ///
+        /// <para><b>Test Identifier</b> - SetFixedContractAmt_String_NormalTest1()</para>
+        /// <para><b>Unique Identifier</b> - AE.E.SFCAD.N.1</para>
+        /// <para><b>Description</b> - Method tests the regular use of the method</para>
+        /// <para><b>Method of execution</b> - Automatic</para>
+        /// <para><b>Input data</b> - "15.00"</para>
+        /// <para><b>Expected outputs</b> - "True"</para>
+        /// <para><b>Observed outputs</b> - passed</para>
+        /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
+        /// 
+
+        [TestMethod]
+        public void SetFixedContractAmt_String_NormalTest1()
+        {
+            string input = "15.00";
+            bool expected = true;
+            bool actual = false;
+
+            ContractEmployee conE = new ContractEmployee();
+            actual = conE.SetFixedContractAmt(input);
+            Assert.AreEqual(expected, actual, "Did not accept valid contract start date");
+
+        }
+        ///
+        /// <para><b>Test Identifier</b> - CheckFixedContractAmt_Decimal_NormalTest1()</para>
+        /// <para><b>Unique Identifier</b> - AE.E.CFCAD.N.1</para>
+        /// <para><b>Description</b> - Method tests the regular use of the method</para>
+        /// <para><b>Method of execution</b> - Automatic</para>
+        /// <para><b>Input data</b> - "15.00"</para>
+        /// <para><b>Expected outputs</b> - "True"</para>
+        /// <para><b>Observed outputs</b> - passed</para>
+        /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
+        /// 
+
+        [TestMethod]
+        public void CheckFixedContractAmt_Decimal_NormalTest1()
+        {
+            Decimal input = 15.00m;
+            bool expected = true;
+            bool actual = false;
+
+            ContractEmployee conE = new ContractEmployee();
+            actual = conE.CheckFixedContractAmt(input);
+            Assert.AreEqual(expected, actual, "Did not accept valid contract start date");
+        }
+
+        ///
+        /// <para><b>Test Identifier</b> - SetFixedContractAmt_Decimal_NormalTest1()</para>
+        /// <para><b>Unique Identifier</b> - AE.E.SFCAD.N.1</para>
+        /// <para><b>Description</b> - Method tests the regular use of the method</para>
+        /// <para><b>Method of execution</b> - Automatic</para>
+        /// <para><b>Input data</b> - "15.00"</para>
+        /// <para><b>Expected outputs</b> - "True"</para>
+        /// <para><b>Observed outputs</b> - passed</para>
+        /// <para><b>If Failed</b> - Displays failed message regarding setting the variable</para>
+        /// 
+
+        [TestMethod]
+        public void SetFixedContractAmt_Decimal_NormalTest1()
+        {
+            Decimal input = 15.00m;
+            bool expected = true;
+            bool actual = false;
+
+            ContractEmployee conE = new ContractEmployee();
+            actual = conE.SetFixedContractAmt(input);
+            Assert.AreEqual(expected, actual, "Did not accept valid contract start date");
+        }
     }
 }
